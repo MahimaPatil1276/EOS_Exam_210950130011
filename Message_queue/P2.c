a390306ce9e09c4f2d55b34a2e82687306649285
MahimaPatil1276 <mahimapatil20998@gmail.com>
2021-12-14 13:22:49 +0530

    done

--- Message_queue/P2.c
+++ Message_queue/P2.c
@@ -2,11 +2,13 @@
 #include<fcntl.h>
 #include<sys/stat.h>
 #include<mqueue.h>
+#include<string.h>
 
 mqd_t cdacmq;
 struct mq_attr cdacmq_attr;
 char buff[128];
 int msg_prio;
+int c;
 
 int main(int argc, char const *argv[])
 {
@@ -16,9 +18,12 @@ int main(int argc, char const *argv[])
     cdacmq_attr.mq_curmsgs = 0;   //number of message currently queued
 
     cdacmq = mq_open("/cdacmq", O_RDONLY | O_CREAT, S_IRUSR | S_IWUSR, &cdacmq_attr);
-
+    
     mq_receive(cdacmq, buff, 128, &msg_prio);
     printf("Received msg: %s\nMsg prio: %d\n", buff, msg_prio);
+
+    c = strlen(buff);
+    printf("Length: %d\n", c);
 
     mq_close(cdacmq);
     return 0;
